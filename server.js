1b753e29ffe161dab0a38cd4a7b1837704e1306a
Yoshiyuki Kondo <cond@st.rim.or.jp>
2013-08-17 17:38:15 +0900

    サーバのメインプログラムserver.jsを追加。
    下記のコマンドで起動される。(ポートは3000)
    
        $ node server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+// Module dependencies.
+
+var
+    application_root = __dirname,
+    express = require('express'),	// Web framework
+    path = require('path'),		// Utilieies for dealing with paths
+    mongoose = require('mongoose');	// MongoDB integration
+
+// Crete server
+var app = express();
+
+// Configure server
+app.configure(function() {
+    // parses request body and populates request.body
+    app.use(express.bodyParser());
+
+    // checks require.body for HTTP method overrides
+    app.use(express.methodOverride());
+
+    // perform route lookup based on url and HTTP method
+    app.use(app.router);
+
+    // where to serve static content
+    app.use(express.static(path.join(application_root, 'site')));
+
+    // show all errors in development
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+// Start server
+var port = 3000;
+app.listen(port, function() {
+    console.log('Express server listening on port %d in %s mode',
+		port, app.settings.env);
+});
