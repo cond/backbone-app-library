4c2d70da1d3124b7f57be5fcd13592917e4441f1
Yoshiyuki Kondo <cond@st.rim.or.jp>
2013-08-18 11:32:22 +0900

    動作確認用のダミーAPI(/api)を追加。

--- server.js
+++ server.js
@@ -33,3 +33,8 @@ app.listen(port, function() {
     console.log('Express server listening on port %d in %s mode',
 		port, app.settings.env);
 });
+
+// Routes (Dummy API)
+app.get('/api', function(request, response)  {
+    response.send('Library API is running');
+});
